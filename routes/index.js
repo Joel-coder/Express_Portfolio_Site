59d69387c2bc765e9e26fcdab3ca78c9012e3d2c
Joel Vargas <joelvargasapolinario@gmail.com>
2021-10-02 11:14:03 -0400

    home page styles

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Home" });
+  res.render("index", { title: "Welcome!" });
 });
 
 /* GET home page. */
 router.get("/home", function (req, res, next) {
-  res.render("index", { title: "Home" });
+  res.render("index", { title: "Welcome!" });
 });
 
 /* GET About Us page. */
